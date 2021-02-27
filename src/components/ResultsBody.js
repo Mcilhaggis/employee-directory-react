3718d27a0752b798f06b637fd739ee645f28b2fc
Rachael McIlhagga <rachael.mcilhagga@live.co.uk>
2021-02-27 15:56:56 -0500

    adding readme

--- src/components/ResultsBody.js
+++ src/components/ResultsBody.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../App.css";
+import "./style.css";
 
 //the curly braces takes things from the state and passes them into the props/ into the components
 
@@ -19,7 +20,7 @@ const ResultsBody = ({image, name, phone, email, dob}) => {
 
     return (
                     <tr>
-                        <td><img src={image} alt=""></img></td>
+                        <td><img src={image} alt="images of employees"></img></td>
                         <td>{name}</td>
                         <td>{phone}</td>
                         <td>{email}</td>
